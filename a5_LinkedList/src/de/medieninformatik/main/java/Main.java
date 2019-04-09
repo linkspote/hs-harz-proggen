44fcd47f32e99114480f286f355db85b6dc99eea
ghKai <45194053+ghKai@users.noreply.github.com>
2019-04-09 18:45:18 +0200

    Add files via upload

---
+++ a5_LinkedList/src/de/medieninformatik/main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * The <code>Main</code> class is used to ...
+ *
+ * @author Kai Gutsmann (m26667)
+ * @author Kevin Kleiber (m26675)
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Starting point of the application. This method is used to ...
+     */
+    public static void main (String[] args) {
+        // ...
+    }
+}
