07a4ad9a0bade24bf663d03948e1be9c06531747
Kevin Kleiber <kevin_kleiber@web.de>
2019-04-15 09:20:18 +0200

    [Feature] Added standard template to use for the following projects

---
+++ src/de/medieninformatik/main/java/Main.java
@@ -0,0 +1,20 @@
+/**
+ * The {@code Main} class contains the main() method which represents the main execution point of each program.
+ *
+ * <p>ATTENTION: This program ist just an example!
+ *
+ * @author Kai Gutsmann (m26667)
+ * @author Kevin Kleiber (m26675)
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * This method is the main execution point of each program.
+     *
+     * @param args Represents a String array which contains parameters to configure the program execution.
+     */
+    public static void main (String[] args) {
+        // Do something else here
+        System.out.println("Hello world!");
+    }
+}
