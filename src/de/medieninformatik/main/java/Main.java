03a4ed0aa8477e79e1e1fb9f3bf608f5163711b3
ghKai <kaigu.gh@gmail.com>
2019-04-29 16:01:57 +0200

    [Feature] Code Cleanup [Feature] Add Unit tests for MyLinkedList [Fix] remove(Object) method wasn't able to find non-null objects

--- src/de/medieninformatik/main/java/Main.java
+++ src/de/medieninformatik/main/java/Main.java
@@ -22,51 +22,66 @@ public class Main {
      * @param args Represents a String array which contains parameters to configure the program execution.
      */
     public static void main (String[] args) {
+        List<String> lsLines = null;
         try {
+            // read file contents into a List of Strings
             Path pFilePath = Paths.get("bsptree.txt");
-            List<String> lsLines = Files.readAllLines(pFilePath);
-
-            Map<Character, Integer> mCharOccurrence = new HashMap<>();
-
-            for (int i = 0; i < lsLines.size(); i++) {
-                String sLineContent = lsLines.get(i);
-
-                for (int j = 0; j < sLineContent.length(); j++) {
-                    char cChar = sLineContent.charAt(j);
-
-                    if (mCharOccurrence.containsKey(cChar)) {
-                        Integer iOccurrence = mCharOccurrence.get(cChar);
-                        iOccurrence++;
-                        mCharOccurrence.put(cChar, iOccurrence);
-                    }
-                    else {
-                        mCharOccurrence.put(cChar, 1);
-                    }
-                }
-            }
-
-            PriorityQueue<Node> pqNode = new PriorityQueue<>(mCharOccurrence.size(), Comparator.comparing(Node::getOccurrence));
-
-            mCharOccurrence.forEach((cChar, iOccurrence) -> pqNode.add(new Node(null, null, iOccurrence, cChar)));
-
-            while (pqNode.size() > 1) {
-                Node nOne = pqNode.poll();
-                Node nTwo = pqNode.poll();
-                pqNode.offer(new Node(nOne, nTwo, null));
-            }
-
-            HashMap<Character, String> mEncoding = new HashMap<>();
-            calcEncoding(pqNode.poll(), "", mEncoding);
+            lsLines = Files.readAllLines(pFilePath);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        // Create HashMap for containing the occurrences of chars
+        Map<Character, Integer> mCharOccurrence = new HashMap<>();
+        // Check all Lists
+        for (int i = 0; i < lsLines.size(); i++) {
+            String sLineContent = lsLines.get(i);
+            // Get each individual char
+            for (int j = 0; j < sLineContent.length(); j++) {
+                char cChar = sLineContent.charAt(j);
+                // char is already in the map, increment occurrence counter accordingly
+                if (mCharOccurrence.containsKey(cChar)) {
+                    Integer iOccurrence = mCharOccurrence.get(cChar);
+                    iOccurrence++;
+                    mCharOccurrence.put(cChar, iOccurrence);
+                }
+                // otherwise, add char and initialise the occurrence counter
+                else {
+                    mCharOccurrence.put(cChar, 1);
+                }
+            }
+        }
+        // Create Priority Queue to help build the tree structure
+        PriorityQueue<Node> pqNode = new PriorityQueue<>(mCharOccurrence.size(), Comparator.comparing(Node::getOccurrence));
+        // Add new Nodes with all characters from the HashMap
+        mCharOccurrence.forEach((cChar, iOccurrence) -> pqNode.add(new Node(null, null, iOccurrence, cChar)));
+        // Create parent Nodes with the Nodes from the Queue to the build a bottom up tree structure
+        while (pqNode.size() > 1) {
+            // Get first two Nodes from the Queue
+            Node nOne = pqNode.poll();
+            Node nTwo = pqNode.poll();
+            // Offer new parent Node containing previous Elements as child Nodes
+            pqNode.offer(new Node(nOne, nTwo, null, nOne.getOccurrence()+nTwo.getOccurrence()));
+        }
+        // HashMap containing every character with its respective encoding
+        HashMap<Character, String> mEncoding = new HashMap<>();
+        calcEncoding(pqNode.poll(), "", mEncoding);
+        // print out the encoding table
+        System.out.println(mEncoding);
     }
 
+    /**
+     * This method is used to traverse the tree structure and save the characters and their encoding in a HashMap.
+     * @param p_nNode Current Node of traversion
+     * @param p_sCurrentEncoding String that saves the encoding of an element in leaf Node
+     * @param p_mEncoding HashMap to store results
+     */
     private static void calcEncoding (Node p_nNode, String p_sCurrentEncoding, HashMap<Character, String> p_mEncoding) {
+        // If the Node is a leaf Node, store its content and its encoding as String
         if (p_nNode.getContent() != null) {
             p_mEncoding.put(p_nNode.getChar(), p_sCurrentEncoding);
         }
+        // check left and right child Nodes
         else {
             calcEncoding(p_nNode.getLeft(), p_sCurrentEncoding + "0", p_mEncoding);
             calcEncoding(p_nNode.getRight(), p_sCurrentEncoding + "1", p_mEncoding);
--- src/de/medieninformatik/main/java/Node.java
+++ src/de/medieninformatik/main/java/Node.java
@@ -1,22 +1,32 @@
 // TODO: 25.04.2019 - Add comments to everything
+
+/**
+ * This Class is used to create <code>Node</code> Elements, which are then used to build a tree data structure.
+ */
 public class Node {
+    // two separate sub nodes
     private Node nLeft, nRight;
+    // Integer containing number of occurrence
     private int iOccurrence;
-    private char cChar;
+    // private char cChar
+    // Content Node of given Node object
     private ContentNode cnContent;
 
+    /**
+     * This class provides a content Node, which stores a character.
+     */
     private class ContentNode {
         private char cChar;
-
         public ContentNode (char p_cChar) {
             cChar = p_cChar;
         }
     }
 
-    public Node (Node p_nLeft, Node p_nRight, ContentNode p_cnContent) {
+    public Node (Node p_nLeft, Node p_nRight, ContentNode p_cnContent, int p_iOccurrence) {
         nLeft = p_nLeft;
         nRight = p_nRight;
         cnContent = p_cnContent;
+        iOccurrence = p_iOccurrence;
     }
 
     public Node (Node p_nLeft, Node p_nRight, int p_iOccurrence, char p_cChar) {
@@ -26,6 +36,11 @@ public class Node {
         cnContent = new ContentNode(p_cChar);
     }
 
+    /**
+     * Returns a textual representation of the <code>Node</code> object, which contains the number of occurrence and the
+     * given character stored in the <code>Node</code>.
+     * @return String representation of given object
+     */
     @Override
     public String toString() {
         return "Knoten {" +
@@ -34,22 +49,42 @@ public class Node {
                 "}";
     }
 
+    /**
+     * Access left sub Node.
+     * @return Reference to the left sub node
+     */
     public Node getLeft() {
         return nLeft;
     }
 
+    /**
+     * Access right sub Node.
+     * @return Reference to the right sub node
+     */
     public Node getRight() {
         return nRight;
     }
 
+    /**
+     * Returns the counter for how often an element is present.
+     * @return Number of occurrence as Integer
+     */
     public int getOccurrence() {
         return iOccurrence;
     }
 
+    /**
+     * Returns the character that is stored in the <code>ContentNode</code>.
+     * @return Stored char in Node
+     */
     public char getChar() {
         return cnContent.cChar;
     }
 
+    /**
+     * Get a reference to the <code>ContentNode</code> object.
+     * @return Reference to Content Node
+     */
     public ContentNode getContent() {
         return cnContent;
     }
